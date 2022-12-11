d5b9984fedcfed867a8d0a95ddb4266beb949873
SnowzNZ <jackson.baylis@outlook.co.nz>
2022-12-11 19:35:58 +1300

    Complete tag generator

--- main.py
+++ main.py
@@ -1,23 +1,70 @@
 from requests_html import HTMLSession
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
-url = "https://www.youtube.com/watch?v=eiRtB7CTxkI"
+s = HTMLSession()
 
-session = HTMLSession()
+query = input("Enter a title or phrase: ")
 
-response = session.get(url)
-# Execute Javascript
-response.html.render(sleep=1)
+split_query = query.split()
 
-soup = bs(response.html.html, "html.parser")
+search_query = "+".join(split_query)
 
-video_meta = {}
+url = f"https://www.youtube.com/results?search_query={search_query}"
 
-video_meta["tags"] = ", ".join(
-    [
-        meta.attrs.get("content")
-        for meta in soup.find_all("meta", {"property": "og:video:tag"})
-    ]
+tags = []
+
+
+def get_videos(url):
+    global video_urls
+    video_urls = []
+
+    search = s.get(url)
+
+    search.html.render(sleep=1)
+
+    videos = search.html.find(
+        "ytd-video-renderer.style-scope.ytd-item-section-renderer"
+    )
+
+    for video in videos:
+        video_url = (
+            "https://www.youtube.com"
+            + video.find("a#video-title", first=True).attrs["href"]
+        )
+
+        video_urls.append(video_url)
+
+
+def get_tags(url):
+    video = s.get(url)
+    video.html.render(sleep=0, timeout=100, keep_page=False, scrolldown=5)
+
+    soup = BeautifulSoup(video.html.html, "html.parser")
+
+    num_chars = 0
+
+    f = soup.find_all("meta", {"property": "og:video:tag"})
+
+    for meta in f:
+        a = meta.attrs.get("content")
+        for element in tags:
+            num_chars += len(element)
+        if num_chars < 400:
+            if a not in tags:
+                tags.append(a)
+            else:
+                pass
+        else:
+            break
+
+
+get_videos(url)
+
+amount = int(
+    input(f"How many videos should be searched for tags out of {len(video_urls)}? ")
 )
 
-print(video_meta)
+for i in range(amount):
+    get_tags(video_urls[i])
+
+print(tags)
