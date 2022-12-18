6dabbd21880fc415776b59a1d4f60bc94bd29d2d
SnowzNZ <jackson.baylis@outlook.co.nz>
2022-12-18 21:05:31 +1300

    Make it more user friendly

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pyperclip
+import lxml
+import cchardet
+import os
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 session = HTMLSession()
 
-query = input("Tag Generation Query: ")
+query = input("Please enter a query or title\n> ")
 
 split_query = query.split()
 
@@ -15,7 +19,7 @@ url = f"https://www.youtube.com/results?search_query={search_query}"
 tags = []
 
 
-def get_videos(url):
+def get_videos(url: str):
     global video_urls
     video_urls = []
 
@@ -27,7 +31,7 @@ def get_videos(url):
         "ytd-video-renderer.style-scope.ytd-item-section-renderer"
     )
 
-    for video in videos:
+    for video in tqdm(videos):
         video_url = (
             "https://www.youtube.com"
             + video.find("a#video-title", first=True).attrs["href"]
@@ -36,11 +40,11 @@ def get_videos(url):
         video_urls.append(video_url)
 
 
-def get_tags(url):
+def get_tags(url: str):
     video = session.get(url)
     video.html.render(sleep=0, timeout=100, keep_page=False, scrolldown=5)
 
-    soup = BeautifulSoup(video.html.html, "html.parser")
+    soup = BeautifulSoup(video.html.html, "lxml")
 
     num_chars = 0
 
@@ -60,21 +64,31 @@ def get_tags(url):
 
 
 get_videos(url)
+print("Scraping videos...")
 
 amount = int(
     input(
-        f"How many top ranking videos should tags be taken from? [#/{len(video_urls)}]: "
+        f"How many top videos out of {len(video_urls)}, do you want to take tags from?\nThe more videos, the longer it will take.\n> "
     )
 )
 
-for i in range(amount):
+print("Scraping tags...")
+for i in tqdm(range(amount)):
     get_tags(video_urls[i])
 
 my_list_str = str(tags).replace("[", "").replace("]", "").replace("'", "")
 
-copy = input("Do you wish to copy the tags to your clipboard? [Y/N]: ")
+os.system("cls" if os.name == "nt" else "clear")
+
+print("Tags:")
+print(my_list_str)
+copy = input("Do you want to copy the tags to the clipboard? [Y/N]\n> ")
 
 if copy.capitalize() == "Y":
     pyperclip.copy(my_list_str)
 else:
     pass
+
+print(
+    "Thanks for using my tool! Make sure to give it a star on GitHub if you liked it!"
+)
